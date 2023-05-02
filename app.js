276e06a59d1011a5616c19c957c6ee834216933b
Dilip Satnere <111310475+Dilipsatnere@users.noreply.github.com>
2023-05-02 15:31:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(
 //         origin:"*"
 //     })
     cors({
-        origin:"https://vast-dog-boot.cyclic.app/user",
-        origin:"http://localhost:4200/signin"  
+//         origin:"https://vast-dog-boot.cyclic.app/user",
+        origin:"http://localhost:4200"  
             
         
     })
